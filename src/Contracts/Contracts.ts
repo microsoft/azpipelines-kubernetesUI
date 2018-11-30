1aacb8f70debcf0556d99382a14afd141323f235
Sandeep Venkata <savenkat@microsoft.com>
2018-11-30 08:44:18 +0530

    Spltting up the repo
    - All the vsix related files will be moved to a separate repo: azPipeline-KubernetesUI-devopsExtension
    https://github.com/Microsoft/azPipeline-KubernetesUI-devopsExtension

--- src/Contracts/Contracts.ts
+++ src/Contracts/Contracts.ts
@@ -14,28 +14,3 @@ export interface IKubeService {
 
     getReplicaSets(): Promise<K8sTypes.V1ReplicaSetList>;
 }
-
-export enum KubeResourceType {
-    Pods = 1,
-    Deployments = 2,
-    Services = 4,
-    ReplicaSets = 8,
-}
-
-export abstract class KubeServiceBase implements IKubeService {
-    getPods(): Promise<K8sTypes.V1PodList> {
-        return this.fetch(KubeResourceType.Pods);
-    }
-
-    getDeployments(): Promise<K8sTypes.V1DeploymentList> {
-        return this.fetch(KubeResourceType.Deployments);
-    }
-    getServices(): Promise<K8sTypes.V1ServiceList> {
-        return this.fetch(KubeResourceType.Services);
-    }
-    getReplicaSets(): Promise<K8sTypes.V1ReplicaSetList> {
-        return this.fetch(KubeResourceType.ReplicaSets);
-    }
-
-    abstract fetch(resourceType: KubeResourceType): Promise<any>;
-}
---
+++ src/Contracts/KubeServiceBase.ts
@@ -0,0 +1,33 @@
+/*
+    Copyright (c) Microsoft Corporation. All rights reserved.
+    Licensed under the MIT license.
+*/
+
+import * as K8sTypes from "@kubernetes/client-node";
+
+import { IKubeService } from "./Contracts";
+
+export enum KubeResourceType {
+    Pods = 1,
+    Deployments = 2,
+    Services = 4,
+    ReplicaSets = 8,
+}
+
+export abstract class KubeServiceBase implements IKubeService {
+    getPods(): Promise<K8sTypes.V1PodList> {
+        return this.fetch(KubeResourceType.Pods);
+    }
+
+    getDeployments(): Promise<K8sTypes.V1DeploymentList> {
+        return this.fetch(KubeResourceType.Deployments);
+    }
+    getServices(): Promise<K8sTypes.V1ServiceList> {
+        return this.fetch(KubeResourceType.Services);
+    }
+    getReplicaSets(): Promise<K8sTypes.V1ReplicaSetList> {
+        return this.fetch(KubeResourceType.ReplicaSets);
+    }
+
+    abstract fetch(resourceType: KubeResourceType): Promise<any>;
+}
