9ed3329321b6762558774c338f4a60afcdb013d4
Madhu V <madhuv@microsoft.com>
2018-12-27 14:47:32 +0530

    updated the test file enumeartion for UTs

--- webpack.tests.config.js
+++ webpack.tests.config.js
@@ -1,22 +1,31 @@
 const path = require("path");
 const fs = require("fs");
+const globSync = require("glob").sync
 
 // Webpack entry points. Mapping from resulting bundle name to the source file entry.
 const entries = {};
 const outputPath = path.resolve(__dirname, "./dist_tests");
 
-// Loop through subfolders in the "pages" folder and add an entry for each one
-const testsDir = path.join(__dirname, "tests");
-fs.readdirSync(testsDir).filter(dir => {
-    if (fs.statSync(path.join(testsDir, dir)).isDirectory()) {
-        entries[dir] = "./" + path.relative(process.cwd(), path.join(testsDir, dir, dir));
+// Loop through subfolders for .test.ts/tsx/js files
+const allFiles = globSync(__dirname + "/tests/**/*");
+let fileCounter = 0;
+allFiles.forEach(f => {
+    if (fs.statSync(f).isFile()) {
+        // find all test files, find their relative path and the name of the file (without extension)
+        // use the filename as the key and relative path for creating the webpack
+        if (f.endsWith(".test.ts") || f.endsWith(".test.tsx") || f.endsWith(".test.js")) {
+            fileCounter++;
+            const relativePath = path.relative(process.cwd(), f);
+            const fName = fileCounter + "_" + path.parse(f).name;
+            entries[fName] = "./" + relativePath;
+        }
     }
 });
 
 module.exports = {
     entry: entries,
     output: {
-        filename: "[name]/[name].js",
+        filename: "[name].js",
         path: outputPath
     },
     resolve: {
