789db3c58d56db519669691d1ed8783c54d2931e
Rajendra Kolli <rakol@microsoft.com>
2019-01-31 16:18:26 +0530

    Adding https call before opening a socket to forceload CA certificate into mem
    Signed-off-by: Rajendra Kolli <rakol@microsoft.com>
    (cherry picked from commit 92958333845ce74cc3d4d07cde7fc5f621476fc0)

--- src/Contracts/TerminalService.ts
+++ src/Contracts/TerminalService.ts
@@ -7,7 +7,8 @@ export class TerminalHandler {
     private token:string;
     private termRef: HTMLElement | null;
     private terminal:Terminal | null = null;
-    private socket:WebSocket | null = null;
+    private socket: WebSocket | null = null;
+    private clusterUrl: string;
     constructor(config:any, termRef: HTMLDivElement | null) {
         const ssl = config.server.startsWith('https://');
         const target  = ssl ? config.server.substr(8) : config.server.substr(7);
@@ -16,6 +17,7 @@ export class TerminalHandler {
         const b64enctoken = btoa(config.token);
         this.token = b64enctoken.substring(0, b64enctoken.indexOf('=')); //stripping the = padding in base64 encoded token
         this.termRef = termRef;
+        this.clusterUrl = config.server;
     }
 
     public generateTTYForPod(podName: string, containerName:string):void {
@@ -24,18 +26,40 @@ export class TerminalHandler {
         const path = '/api/v1/namespaces/default/pods/'+podName+'/exec?stdin=1&stdout=1&stderr=1&tty=1&command='+encodeURIComponent("/bin/bash")+'&container='+containerName;
         const uri = `${this.server}${path}`;
         //todo: service account already provides token in base64encoded format. Account for the token from service account
-        protocols[0] = protocols[0]+this.token;
+        protocols[0] = protocols[0] + this.token;
+        
+        if (this.clusterUrl.startsWith('https://')) {
+            //requesting the url once to let chrome cache CA for the session and then try opening socket
+            fetch(this.clusterUrl, {
+                mode: "no-cors",
+                method:"GET"
+            }).then(res => {
+                //ignore the response since it will be a 401. CA will be loaded in mem for this session
+                //so attempt to open the socket now
+                this.openSocket(uri, protocols);
+            }).catch(err => {
+                //this suggests errors like network unreachable etc. Use this error to let user know that term cannot be opened
+                console.log(err);
+            })
+        } else {
+            //open plain http socket directly
+            this.openSocket(uri, protocols);
+        }
+    
+    }
+
+    private openSocket(uri: string, protocols: string[]) {
         const sock = new WebSocket(uri, protocols);
         sock.onopen = () => {
             this.socket = sock;
-            if(this.termRef) {
+            if (this.termRef) {
                 this.terminal = new Terminal({
-                    cols:180,
-                    rows:30,
-                    fontSize:14,
-                    cursorBlink:true,
+                    cols: 180,
+                    rows: 30,
+                    fontSize: 14,
+                    cursorBlink: true,
                 });
-                this.terminal.on('data', (data)=>{
+                this.terminal.on('data', (data) => {
                     this.transferData(data);
                 });
                 this.terminal.open(this.termRef);
@@ -44,7 +68,7 @@ export class TerminalHandler {
             }
         };
 
-        sock.onerror = (err) =>{
+        sock.onerror = (err) => {
             console.log(err);
             this.socket = null;
             this.terminal && this.terminal.dispose();
@@ -57,15 +81,15 @@ export class TerminalHandler {
             this.terminal = null;
         }
 
-        sock.onmessage = (event) =>{
+        sock.onmessage = (event) => {
             const reader = new FileReader();
-            reader.onload = () =>{
-                if(reader.result) {
-                    if(reader.result instanceof ArrayBuffer) {
+            reader.onload = () => {
+                if (reader.result) {
+                    if (reader.result instanceof ArrayBuffer) {
                         const buff = Buffer.from(reader.result);
                         //refitting the terminal to actual dimensions
                         this.terminal && this.terminal.write(buff.slice(1).toString('utf-8'));
-                    }else {
+                    } else {
                         //this condition should not be seen ever
                         console.log(typeof reader.result);
                     }
