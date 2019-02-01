23935a36a4561f0ccea3466384e4bb3548e1e710
Rajendra Kolli <rakol@microsoft.com>
2019-02-01 10:51:10 +0530

    Moving from ToolTipHost to ToolTip
    Signed-off-by: Rajendra Kolli <rakol@microsoft.com>

--- src/WebUI/Components/ResourceStatusComponent.tsx
+++ src/WebUI/Components/ResourceStatusComponent.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { BaseComponent } from "@uifabric/utilities/lib";
 import { IStatusProps, Status, StatusSize } from "azure-devops-ui/Status";
 import { IVssComponentProperties } from "../Types";
-import { TooltipHost } from "azure-devops-ui/Tooltip";
+import { Tooltip } from "azure-devops-ui/TooltipEx";
 
 export interface IResourceStatusProps extends IVssComponentProperties {
     statusProps: IStatusProps | undefined;
@@ -15,16 +15,33 @@ export interface IResourceStatusProps extends IVssComponentProperties {
 export class ResourceStatusComponent extends BaseComponent<IResourceStatusProps, {}> {
 
     public render(): React.ReactNode {
+        if (this.props.toolTipText) {
+            return (
+                <div className="kube-status-container">
+                    {this._getStatusWithToolTip()}
+                </div>
+            );
+        }
+        return this._getStatus();
+    }
+
+    private _getStatusWithToolTip(): React.ReactNode {
+        return (
+            <Tooltip text={this.props.toolTipText} overflowOnly={false} showOnFocus={true}>
+                {this._getStatus()}
+            </Tooltip>
+        );
+    }
+
+    private _getStatus(): React.ReactNode {
         return (
             <div className="kube-status-container">
                 {
-                    this.props.toolTipText ?
-                        <TooltipHost content={this.props.toolTipText} >
-                            {this._getStatus()}
-                        </TooltipHost> : this._getStatus()
+                    this.props.statusProps &&
+                    <Status {...this.props.statusProps} size={this.props.statusSize || StatusSize.m} />
                 }
                 {
-                    this.props.statusDescription && 
+                    this.props.statusDescription &&
                     <span className="kube-status-desc">{this.props.statusDescription}</span>
                 }
                 {
@@ -32,10 +49,5 @@ export class ResourceStatusComponent extends BaseComponent<IResourceStatusProps,
                 }
             </div>
         );
-    }
-
-    private _getStatus(): React.ReactNode {
-        return this.props.statusProps &&
-            <Status {...this.props.statusProps} size={this.props.statusSize || StatusSize.m} />;
     }
 }
