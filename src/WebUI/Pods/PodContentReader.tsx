d458bd386f7bdc4e1e087416778737ce1e513c30
madhu vadlapudi <madhuv@microsoft.com>
2019-04-15 19:52:15 +0530

    consume logreader from devops (#169)

--- src/WebUI/Pods/PodContentReader.tsx
+++ src/WebUI/Pods/PodContentReader.tsx
@@ -4,8 +4,6 @@
 */
 
 import { BaseComponent } from "@uifabric/utilities";
-import { CardContent, CustomCard } from "azure-devops-ui/Card";
-import { css } from "azure-devops-ui/Util";
 import * as React from "react";
 import { getContentReaderComponent } from "../Common/KubeConsumer";
 import { IVssComponentProperties } from "../Types";
@@ -17,16 +15,7 @@ export interface IPodReaderProps extends IVssComponentProperties {
 }
 
 export class PodContentReader extends BaseComponent<IPodReaderProps> {
-    public render(): JSX.Element {
-        return (
-            // monaco-editor class added here to have the same theme as monaco.
-            <CustomCard className={css(this.props.className || "", "bolt-table-card", "monaco-editor", "k8s-card-padding", "flex-grow")}>
-                <CardContent className={css(this.props.contentClassName || "", "reader-content")} contentPadding={false}>
-                    <div className="reader-outer" style={{ width: "100%", height: "500px", position: "relative" }}>
-                        {getContentReaderComponent({ ...this.props })}
-                    </div>
-                </CardContent>
-            </CustomCard>
-        );
+    public render(): React.ReactNode {
+        return getContentReaderComponent({ ...this.props });
     }
 }
