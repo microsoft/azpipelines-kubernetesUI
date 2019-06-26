17e531275ee8dee44f49c59fd74de1129c12cfc1
dabasnidhi <nidabas@microsoft.com>
2019-06-26 11:17:54 +0530

    Updated l0 as per review

--- tests/WebUI/ActionsCreatorTests/ImageDetailsActionsCreator.test.ts
+++ tests/WebUI/ActionsCreatorTests/ImageDetailsActionsCreator.test.ts
@@ -8,49 +8,53 @@ import { MockImageService } from "../MockImageService";
 
 describe("ImageDetailsActionsCreator getImageDetails Tests", () => {
     const mockImageDetails: IImageDetails = {
-        imageName: "test-image",
-        imageUri: "test-image-uri",
-        hash: "test-image-hash",
-        baseImageName: "test-image-baseImageName",
-        imageType: "test-image-imageType",
-        mediaType: "test-image-mediaType",
+        imageName: "https://k8s.gcr.io/coredns@sha2563e2be1cec87aca0b74b7668bbe8c02964a95a402e45ceb51b2252629d608d03a",
+        imageUri: "https://k8s.gcr.io/coredns@sha2563e2be1cec87aca0b74b7668bbe8c02964a95a402e45ceb51b2252629d608d03a",
+        hash: "294f0bb6fe38fe0ab9b7ac8c6db39c2054ba038a6fe53a7cafbc20829d54a424",
+        baseImageName: "k8s.gcr.io/coredns23",
+        imageType: "",
+        mediaType: "",
         tags: ["test-image-tag"],
         layerInfo: [{ "directive": "ADD", "arguments": "mock argument", "createdOn": new Date("2019-06-25T05:50:11+05:30"), "size": "88.9MB" }],
         runId: 1,
-        pipelineVersion: "test-image-pipelineVersion",
+        pipelineVersion: "20",
         pipelineName: "test-image-pipelineName",
         pipelineId: "111",
         jobName: "test-image-jobName",
         imageSize: "1000MB",
     };
 
-    it("If imageDetails are not defined in store then call image service", () => {
-        // Mock return value for image service getImageDetails
-        const mockImageService = new MockImageService();
-        KubeFactory.getImageService = jest.fn().mockReturnValue(mockImageService);
-        mockImageService.getImageDetails = jest.fn().mockReturnValue(new Promise(() => mockImageDetails));
+    let mockImageService = new MockImageService();
+    KubeFactory.getImageService = jest.fn().mockReturnValue(mockImageService);
 
-        const mockShowImageDetailsAction = jest.fn().mockImplementation((imageDetails: IImageDetails): void => { });
+    const mockShowImageDetailsAction = jest.fn().mockImplementation((imageDetails: IImageDetails): void => { });
+
+    let imageDetailsStore = StoreManager.GetStore<ImageDetailsStore>(ImageDetailsStore);
+
+    beforeEach(() => {
+        // Mock return value for image service getImageDetails
+        mockImageService.getImageDetails = jest.fn().mockReturnValue(new Promise(() => mockImageDetails));
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("If imageDetails are not defined in store then call image service", () => {
         // mock imageDetailsStore.getImageDetails
-        const imageDetailsStore = StoreManager.GetStore<ImageDetailsStore>(ImageDetailsStore);
         imageDetailsStore.getImageDetails = jest.fn().mockReturnValueOnce(undefined);
 
         ActionsCreatorManager.GetActionCreator<ImageDetailsActionsCreator>(ImageDetailsActionsCreator).getImageDetails("test-image", mockShowImageDetailsAction);
         expect(mockImageService.getImageDetails).toHaveBeenCalledWith("test-image");
+        expect(mockShowImageDetailsAction).not.toBeCalled(); // Not to be called as image details mock value is undefined here
     });
 
     it("If image details are present in store then do not call image service", () => {
-        // Mock return value for image service getImageDetails
-        const mockImageService = new MockImageService();
-        KubeFactory.getImageService = jest.fn().mockReturnValue(mockImageService);
-        mockImageService.getImageDetails = jest.fn().mockReturnValue(new Promise(() => mockImageDetails));
-
-        const mockShowImageDetailsAction = jest.fn().mockImplementation((imageDetails: IImageDetails): void => { });
         // mock imageDetailsStore.getImageDetails
-        const imageDetailsStore = StoreManager.GetStore<ImageDetailsStore>(ImageDetailsStore);
         imageDetailsStore.getImageDetails = jest.fn().mockReturnValueOnce(mockImageDetails);
-        
+
         ActionsCreatorManager.GetActionCreator<ImageDetailsActionsCreator>(ImageDetailsActionsCreator).getImageDetails("test-image", mockShowImageDetailsAction);
         expect(mockImageService.getImageDetails).not.toBeCalled();
+        expect(mockShowImageDetailsAction).toBeCalledWith(mockImageDetails);
     });
 });
